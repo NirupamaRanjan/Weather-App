c75b595e692906805981549e738f6ca766a3f103
NirupamaRanjan <nirupamaranjan2414@gmail.com>
2021-09-01 01:12:10 +0530

    first commit

--- Components/Map.js
+++ Components/Map.js
@@ -2,9 +2,5 @@ import React from "react";
 import { View, Text } from "react-native";
 
 export default function Map() {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+  return <View></View>;
 }
